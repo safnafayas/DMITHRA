83b1d50b9fb05bb6f9c321e56801146354a8e293
safnafayas <safnafayas22@gmail.com>
2023-08-04 11:49:52 +0530

    3/08 commits

--- user_Register.php
+++ user_Register.php
@@ -3,9 +3,8 @@ include "connect.php";
 $UDID=$_POST['UDID'] ?? '';
 $password=$_POST['password'] ?? '';
 $name=$_POST['name'] ?? '';
-$age=$_POST['age'] ?? '';
 $contact_number=$_POST['contact_number'] ?? '';
-$email=$_POST['email'] ?? '';
+
 
 // $image = $_FILES['profile_image']['name'] ?? '';
 // $imagePath = 'profileImageUpload/' . $image;
@@ -17,7 +16,7 @@ $type=$_POST['type'];
 $sql1=mysqli_query($con,"INSERT INTO  `user_login` (UDID,password,type) VALUES ('$UDID','$password','$type');");
 $userId=mysqli_insert_id($con);
 
-$sql2=mysqli_query($con,"INSERT into `user_register_table`(name,age,contact_number,email,login_id)VALUES('$name','$age','$contact_number','$email','$userId'); ");
+$sql2=mysqli_query($con,"INSERT into `user_register_table`(name,contact_number,login_id)VALUES('$name','$contact_number','$userId'); ");
 
 if($sql1 && $sql2){
   $myarray['result']='success';
